1d74a2baf0718d2c1d553e50be164bd042c2ea09
Taylor Treece <taylor.treece@protonmail.com>
2021-03-09 16:18:41 -0600

    commit tree e27a28ce29fb87bee8562547b4340bbaa2a7d1cd

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,9 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+    def initialize(args)
+        @name = args[:name]
+        @weight = args[:weight]
+        @height = args[:height]
+    end
+
 end
